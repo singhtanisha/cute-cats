b96ceaf527ada3fb64183a3cedd032763af18def
Tanisha Singh <tanishasb@gmail.com>
2019-05-08 21:17:37 -0700

    code style/syntax clean up

--- app/components/image-tile.js
+++ app/components/image-tile.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-    classNames:['image-row'],
+    classNames:['cat-tiles--inline-display']
 });
--- app/controllers/full-screen.js
+++ app/controllers/full-screen.js
@@ -2,7 +2,7 @@ import Controller, { inject as controller } from '@ember/controller';
 import { computed } from '@ember/object';
 
 export default Controller.extend({
-    fullUrl: computed('model', function() {
+    formattedImgurLink: computed('model', function() {
         const model = this.get('model');
         if (model) {
             return `https://i.imgur.com/${model.imageId}.jpg`
